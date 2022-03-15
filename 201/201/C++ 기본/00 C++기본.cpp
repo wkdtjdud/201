eb2cec77d5937eae3b98a0a0796c19aafe1f93ac
seoyoung <wkdtjdud312@gmail.com>
2022-03-15 12:15:40 +0900

    namespace로 축약

--- 201/201/C++ 기본/00 C++기본.cpp
+++ 201/201/C++ 기본/00 C++기본.cpp
@@ -1,8 +1,13 @@
 //std::cout를 포함
 #include <iostream>
+using namespace std; //std::를 생략
+
 
 int main(void)
 {
-	std::cout<<"빠이 \n";
+
+	//std::cout객체로 문자열을 출력
+	//std::endl객체로 개행처리
+	cout << "빠이 \n" << endl;
 	return 0;
 }
