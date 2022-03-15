ada39b1433a2101c61dff7c3a17181807523adcc
seoyoung <wkdtjdud312@gmail.com>
2022-03-15 12:01:51 +0900

    C++기본

---
+++ 201/201/C 기본/05 논리연산자 - 복사본.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+
+	if (++a || ++b && ++c)
+	{
+		printf("Âü");
+	}
+
+	printf("%d %d %d\n", a, b, c);
+	
+	return 0;
+}
--- imsi/imsi/소스.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	printf("Hello World\n");
-	return 0; //return 한 이유 정수니까, 정수 값을 반환
-}
