aaa59b0f0e8acc5a4e246d6b0cd490d2d90387d1
seoyoung <wkdtjdud312@gmail.com>
2022-03-18 10:22:37 +0900

    값이 바뀜

--- 201/201/C 기본/12 구조체 포인터.c
+++ 201/201/C 기본/12 구조체 포인터.c
@@ -6,7 +6,7 @@ typedef struct
 	float y;
 }Point;
 
-void AddValueToPoint(Point p);
+void AddValueToPoint(Point* p);
 
 int main(void)
 {
@@ -14,15 +14,15 @@ int main(void)
 	point.x = 5.0f;
 	point.y = 10.0f;
 	// call by value라서 값이 안 바뀜
-	AddValueToPoint(point);
+	AddValueToPoint(&point);
 	printf("%1.f, %1.f\n", point.x, point.y);
 
 
 	return 0;
 }
 
-void AddValueToPoint(Point p)
+void AddValueToPoint(Point* p)
 {
-	p.x += 10.0f;
-	p.x += 15.0f;
+	p->x += 10.0f;
+	p->y += 15.0f;
 }
