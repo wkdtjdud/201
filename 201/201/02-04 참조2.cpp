8a05b1d0d8d74d6b462ce046ee68e49eb2920484
seoyoung <wkdtjdud312@gmail.com>
2022-04-05 11:37:59 +0900

    call by 레퍼런스(참조)

--- 201/201/02-04 참조2.cpp
+++ 201/201/02-04 참조2.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 using namespace std;
 
-void plus2(int* num)
+void plus2(int& num)
 
 {
-	// num이 포인터 변수이기에 *로 써서 내부값을 처리
-	*num += 2;
+	// num이 참조형 변수이기에 *로 안 써서 내부값을 처리
+	num += 2;
 }
+
+
+
 
 int main(void)
 {
 	int a = 3;
 	// 포인터 매개변수 num에게 주소값으로 전달
-	plus2(&a);
+	plus2(a);
 	cout << a << endl;
 }
