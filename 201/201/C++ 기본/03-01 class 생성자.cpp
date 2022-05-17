26eb4e5c8bd8d1b18f486d50733fd5d8ebb07f14
seoyoung <wkdtjdud312@gmail.com>
2022-05-17 11:19:05 +0900

    므어엉

--- 201/201/C++ 기본/03-01 class 생성자.cpp
+++ 201/201/C++ 기본/03-01 class 생성자.cpp
@@ -1,8 +1,8 @@
 #include <iostream> //cout, endl 사용
-#include <string>
+#include <string.h> //strcpy()사용
 using namespace std; //std::를 생략
 
-/*class Student {
+class Student {
 public:
 
 	Student();
@@ -21,35 +21,43 @@ Student::Student() {
 
 //멤버변수를 초기화 할 수 있으며
 //const형 변수와 참조형 변수를 멤버변수로 할 수 있다.
-Student::Student(int Hakbun, string Name)
-:nHakbun(Hakbun), sName(Name) //멤버변수(매개변수)
+Student::Student(int Hakbun, const char* Name)
+	:nHakbun(Hakbun) //멤버변수(매개변수)
 {
 	
-	cout << "학번이 등록되었습니다." << endl;
+	cout << "일반생성자 호출." << endl;
+	int len = strlen(Name) + 1;	// 공간의 개수 파악
+	sName = new char[len];		// 개수만큼 메모리 할당
+	strcpy(sName, Name);
 }
 
 void Student::show()
 {
 	cout << "학번은 " << nHakbun << "입니다" << endl;
 	cout << "이름은 " << sName << "입니다" << endl << endl;
-} */
+} 
 
 int main(void)
 {
-	/*student stu1 = student();
-	stu1.show();
 
-	student stu2 = student(1111, "jwp");
-	stu2.show(); */
+	Student stu1 = Student(1111, "jwp");
+	stu1.show(); 
 
 	
-	int* ptr2 = new int[4];
+	/*int* ptr2 = new int[4];
 	for (int i = 0; i < 4; i++) {
 		ptr2[i] = 10 + i;
 		cout << ptr2[i] << " ";
 
 	}
-	delete[]ptr2;
+	delete[]ptr2;*/
+
+	/*Student* stu2 = new Student[6];
+	for(int i=0; i<6; i++)
+	stu2[i].show();
+
+	delete []stu2;
+	*/
 
 	return 0;
 
